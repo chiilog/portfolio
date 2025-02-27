638541856f66aa71bd75ffaa6bdbf9e2d8b30d45
chiilog <purgexpurge@gmail.com>
2025-02-27 15:10:53 +0900

    Add React integration and update project configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,32 @@
 import eslintPluginAstro from "eslint-plugin-astro";
-import eslintConfigPrettier from 'eslint-config-prettier';
+import typescriptParser from "@typescript-eslint/parser";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
-  ...eslintPluginAstro.configs.recommended,
-  eslintConfigPrettier,
   {
-    files: ["src/**/*.{astro,ts,tsx,jsx}"],
+    ignores: [
+      "node_modules",
+      ".github",
+      "dist",
+      "public",
+      "package-lock.json",
+      "yarn.lock",
+      "vite.config.ts",
+      "next.config.js",
+      "tsconfig.json",
+      "src/env.d.ts",
+      "*.cjs",
+      "*.mjs",
+    ],
   },
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ["src/**/*.{ts,tsx,jsx}"],
+    languageOptions: {
+      parser: typescriptParser,
+    },
+  },
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
 ];
